38b8f38c49f6dc702c50ca6f74065a80496190ed
xhuljana1 <xhuljana_muskaj@yahoo.com>
2019-05-09 01:00:34 +0200

    Updates

---
+++ precache-manifest.71c2727bc1f38ba2bd416240a2ccc169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8481811b32670f360982ec247e06c0",
+    "url": "/metronome/index.html"
+  },
+  {
+    "revision": "646cfe62777923af7277",
+    "url": "/metronome/static/css/main.1866fed6.chunk.css"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/metronome/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "646cfe62777923af7277",
+    "url": "/metronome/static/js/main.eb918b03.chunk.js"
+  },
+  {
+    "revision": "f67bd54e6e39f1154f66",
+    "url": "/metronome/static/js/runtime~main.17634b44.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
